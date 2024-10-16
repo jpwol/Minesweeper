9b54aa3f50c024aadc9d428d03b61d31912b4bec
jpwol <jpwolfe1981@gmail.com>
2024-10-16 01:41:45 -0400

    Cleaning up for dynamic distribution

--- include/Game.h
+++ include/Game.h
@@ -1,39 +1,38 @@
 #pragma once
-#include <SDL.h>
 #include "Board.h"
+#include <SDL2/SDL.h>
 #include <bitset>
-enum Signal : uint8_t{
-    Zero,
-    Button1,
-    Button2,
-    Button3,
-    ButtonR
-};
+enum Signal : uint8_t { Zero, Button1, Button2, Button3, ButtonR };
 inline Board board;
-class Game{
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    
-    bool running = true;
-    float desiredDensity = 20.625;
-    bool showTiles = false;
-    int bombIndex;
+class Game {
+  SDL_Window *window;
+  SDL_Renderer *renderer;
 
-    int mouseState;
-    int mouseX, mouseY;
-    bool bDown;
-    bool loseState = false;
-    public:
-        Game(int rows, int cols){initGame(rows, cols);}
-        ~Game(){cleanup();}
-        void handleResize(int r, int c, float dD){desiredDensity = dD; cleanup(); initGame(r, c);}
-        void initGame(int rows, int cols);
-        void run();
-        void handleEvents();
-        void handleInput();
-        void update();
-        void render();
-        void cleanup();
+  bool running = true;
+  float desiredDensity = 20.625;
+  bool showTiles = false;
+  int bombIndex;
 
-        Signal signal;
-}; 
+  int mouseState;
+  int mouseX, mouseY;
+  bool bDown;
+  bool loseState = false;
+
+public:
+  Game(int rows, int cols) { initGame(rows, cols); }
+  ~Game() { cleanup(); }
+  void handleResize(int r, int c, float dD) {
+    desiredDensity = dD;
+    cleanup();
+    initGame(r, c);
+  }
+  void initGame(int rows, int cols);
+  void run();
+  void handleEvents();
+  void handleInput();
+  void update();
+  void render();
+  void cleanup();
+
+  Signal signal;
+};
--- include/TextureManager.h
+++ include/TextureManager.h
@@ -1,16 +1,19 @@
 #pragma once
-#include <SDL_image.h>
 #include "Game.h"
+#include <SDL2/SDL_image.h>
 
-class TextureManager{
-    static inline const int tileSize = 32;
-    static inline std::unordered_map<std::string, SDL_Texture*> textures;
-    
-    public:
-        static void hashTexture(const std::string& key, const std::string& path, SDL_Renderer* renderer);
-        static void loadTextures(SDL_Renderer* renderer);
-        static SDL_Texture* getTexture(const std::string& key);
-        static void handleTileRender(SDL_Renderer* renderer, Board& board, bool loseState);
-        static void handleUIRender(SDL_Renderer* renderer, int bombIndex, Signal signal);
-        static void cleanTextures();
+class TextureManager {
+  static inline const int tileSize = 32;
+  static inline std::unordered_map<std::string, SDL_Texture *> textures;
+
+public:
+  static void hashTexture(const std::string &key, const std::string &path,
+                          SDL_Renderer *renderer);
+  static void loadTextures(SDL_Renderer *renderer);
+  static SDL_Texture *getTexture(const std::string &key);
+  static void handleTileRender(SDL_Renderer *renderer, Board &board,
+                               bool loseState);
+  static void handleUIRender(SDL_Renderer *renderer, int bombIndex,
+                             Signal signal);
+  static void cleanTextures();
 };
